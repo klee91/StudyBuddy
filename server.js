d8055c4d8ebc52d136b09d188904ee45296a9ac7
Kevin Lee <lee.kev1991@gmail.com>
2017-05-03 20:58:05 -0400

    html routes and link fixes

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static("./public"));
 
 // Routes =============================================================
 
-// require("./routes/html-routes.js")(app);
+require("./routes/html-routes.js")(app);
 // require("./routes/post-api-routes.js")(app);
 // require("./routes/author-api-routes.js")(app);
 
